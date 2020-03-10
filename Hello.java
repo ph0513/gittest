b751b8bd741a0838958adee439c775d2b61f8322
ph0513 <ph0513@naver.com>
2020-03-10 10:07:47 +0900

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{
-	public static void main(String[] args){
+	public static void main(String[ ] args){
 		System.out.println("Hello");
 		System.out.println("World");
+		System.out.println("안녕하세요");
 	}
 }
